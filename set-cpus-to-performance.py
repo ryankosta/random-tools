b600bf560eaa6bd3706853057300bc1e0ea46f1b
Ryan Kosta <rkosta@ucsd.edu>
2025-08-07 16:40:59 -0700

    add set cpus to performance

---
+++ set-cpus-to-performance.py
@@ -0,0 +1,17 @@
+#!/bin/env python3
+import os
+NUM_CPU=96 #TODO make dynamic
+def get_scaling_dir(cpunum:int):
+    return f"/sys/devices/system/cpu/cpu{cpunum}/cpufreq/scaling_governor"
+def check_setup():
+    if not os.path.isfile(get_scaling_dir(0)):
+        print("WARN: Power scaling settings not properly configured (ie: cpufreq directory DNE)")
+        exit()
+
+        
+def set_all():
+    for cpunum in range(NUM_CPU):
+        cmd = "echo performance > " + get_scaling_dir(cpunum)
+        os.system(cmd)
+check_setup()
+set_all()
